a35144cd0ff7568c8865c7ac02e4436d8dc25abb
Brian Williamson <brianw26@uw.edu>
2020-04-08 08:19:52 -0700

    remove beagles, change style of reference to paper

--- code/sims/lowdim/shell/submit_sim_cv.sh
+++ code/sims/lowdim/shell/submit_sim_cv.sh
@@ -2,6 +2,6 @@
 module load R/3.4.2-foss-2016b-fh1 # edit this line for your particular cluster
 
 ## first the alternative
-sbatch -M beagle --array=1-560 --time=8:00:00 ./call_sim_cv.sh 0
+sbatch --array=1-560 --time=8:00:00 ./call_sim_cv.sh 0
 ## now the null
-sbatch -M beagle --array=1-560 --time=8:00:00 ./call_sim_cv.sh 1
+sbatch --array=1-560 --time=8:00:00 ./call_sim_cv.sh 1
--- code/sims/moddim/shell/submit_sim_mod.sh
+++ code/sims/moddim/shell/submit_sim_mod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 module load Python/2.7.14-foss-2016b-fh1 # edit this line for your particular cluster
 
-sbatch -M beagle --array=1-400 --time=20:00:00 ./call_sim_mod.sh
+sbatch --array=1-400 --time=20:00:00 ./call_sim_mod.sh
